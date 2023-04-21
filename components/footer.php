00c6132facbd35ab79234373da967fea9482a94a
Mahmoud Hmidan <115027511+mahmoudhmidan@users.noreply.github.com>
2023-04-21 11:58:16 -0700

    Add files via upload

---
+++ components/footer.php
@@ -0,0 +1,35 @@
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <a href="tel:1234567890"><i class="fas fa-phone"></i> +123-456-7890</a>
+         <a href="tel:1112223333"><i class="fas fa-phone"></i> +111-222-3333</a>
+         <a href="mailto:mahmoudhmidan99@gmail.com"><i class="fas fa-envelope"></i> mahmoudhmidan99@gmail.com</a>
+         <a href="mailto:mahmoudhmidan99@gmail.com"><i class="fas fa-envelope"></i> mahmoudhmidan99@gmail.com</a>
+         <a href="#"><i class="fas fa-map-marker-alt"></i> mumbai, india - 400104</a>
+      </div>
+
+      <div class="box">
+         <a href="#home">home</a>
+         <a href="#about">about</a>
+         <a href="bookings.php">my bookings</a>
+         <a href="#reservation">reservation</a>
+         <a href="#gallery">gallery</a>
+         <a href="#contact">contact</a>
+         <a href="#reviews">reviews</a>
+      </div>
+
+      <div class="box">
+         <a href="#">facebook <i class="fab fa-facebook-f"></i></a>
+         <a href="#">twitter <i class="fab fa-twitter"></i></a>
+         <a href="#">instagram <i class="fab fa-instagram"></i></a>
+         <a href="#">linkedin <i class="fab fa-linkedin"></i></a>
+         <a href="#">youtube <i class="fab fa-youtube"></i></a>
+      </div>
+
+   </div>
+
+   <div class="credit">&copy; copyright @ 2022 by mr. Mahmoud Hmidan | all rights reseved!</div>
+
+</section>
---
+++ components/user_header.php
@@ -0,0 +1,16 @@
+<section class="header">
+    <div class="flex">
+        <a href="#home" class="logo">Hotels And Resorts</a>
+        <a href="#availability" class="btn">check availability</a>
+        <div class="menu-btn" class="fas fa-bars"></div>
+    </div>
+
+    <nav class="navbar">
+        <a href="index.php#home">home</a>
+        <a href="index.php#about">about</a>
+        <a href="index.php#reservation">reservation</a>
+        <a href="index.php#contact">contact</a>
+        <a href="index.php#reviews">reviews</a>
+        <a href="bookings.php">my bookings</a>
+    </nav>
+</section>
